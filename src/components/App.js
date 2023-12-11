f7fea40f3586eac19fe066e503adc6ecd9815bc8
Dylan Kelly <init.defaultBranch>
2023-12-11 16:06:48 -0500

    commit tree 81230bd520a3effa4e2564516ecff6dd778025b2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
 import video from "../data/video.js";
 
 function App() {
   console.log("Here's your data:", video);
+
+  const [upvotes, setUpvotes] = useState(video.upvotes);
+  const [downvotes, setDownvotes] = useState(video.downvotes);
+  const [comments, setComments] = useState(video.comments);
+
+  const handleUpvote = () => {
+    setUpvotes(upvotes +1);
+  };
+
+  const handleDownvote = () => {
+    setUpvotes(downvotes +1);
+  };
+
+  const handleComments = () => {
+    setComments(!comments);
+  };
+
 
   return (
     <div className="App">
@@ -13,6 +31,16 @@ function App() {
         allowFullScreen
         title="Thinking in React"
       />
+      <header>
+      React Today and Tomorrow and 90% Cleaner React With Hooks
+      </header>
+      <button onClick={handleUpvote}>👍 {upvotes}</button>
+      <button onClick={handleDownvote}>👎 {downvotes}</button>
+      <button onClick={handleComments}> {comments ? 'Hide Comments' : 'Show Comments'}</button>
+
+
+
+      
     </div>
   );
 }
